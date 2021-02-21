5a905fc0e7c30278c15c44fa6b407215a7011850
Katie Blalock <katieblalock314@gmail.com>
2021-02-21 16:13:55 -0700

    Updates

---
+++ precache-manifest.c7cd4e7fdbfa83d9296cede3a1f4c61f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e4abadc7ddc273976f0e2b6d287d1c",
+    "url": "/react-template1/index.html"
+  },
+  {
+    "revision": "93045d09b57d2e74801c",
+    "url": "/react-template1/static/css/main.01f5d8b6.chunk.css"
+  },
+  {
+    "revision": "a42541a06cc3e04549b1",
+    "url": "/react-template1/static/js/2.7c8eff5f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-template1/static/js/2.7c8eff5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93045d09b57d2e74801c",
+    "url": "/react-template1/static/js/main.caf43cc4.chunk.js"
+  },
+  {
+    "revision": "1afb84473552a4a263cf",
+    "url": "/react-template1/static/js/runtime-main.293ea450.js"
+  }
+]);
